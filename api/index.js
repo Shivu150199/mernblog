3e870be09a1fd436ee374a05f871dd4b4055f9ef
shivamsingh <shivamsinghgautam397@gmail.com>
2024-06-14 13:32:12 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import postRouter from './route/post.route.js'
 import commentRouter from './route/comment.router.js'
-
+import path from 'path'
 dotenv.config()
 const app = express()
 
@@ -15,9 +15,18 @@ app.use(cors())
 
 app.use(express.json())
 app.use(cookieParser())
+
+const __dirname=path.resolve()
+
 app.use('/api/auth/v1', authRouter)
 app.use('/api/post/v1', postRouter)
 app.use('/api/comment/v1', commentRouter)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 //middleware for error hadnling
 app.use((err, req, res, next) => {
